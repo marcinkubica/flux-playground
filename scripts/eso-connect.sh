2ae3037fd5f4803e5a3fa4c6c2604de7abee9932
Marcin Kubica <marcin.kubica@engineerbetter.com>
2023-01-12 11:35:09 +0100

    add prometheus

---
+++ scripts/eso-connect.sh
@@ -0,0 +1,3 @@
+gcloud auth login && gcloud auth application-default login && \
+
+gcloud container clusters get-credentials kubernetes-cluster-eso --zone europe-west2-a --project kubernetes-cluster-eso
